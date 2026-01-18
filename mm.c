2b836a5cc7100e48a60b4f5c8411a06945ac93ec
Wanja Staempfli <@student.ethz.ch>
2026-01-18 07:49:44 +0000

    implemented free and coalesce

--- mm.c
+++ mm.c
@@ -22,17 +22,67 @@
 #define ALIGNMENT 8
 #define WSIZE 4             /* word size */
 #define DSIZE 8             /* doubleword size */
+#define CHUNKSIZE (1<<12)   /* Extend heap by this amount (bytes) */
+
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
 #define PACK(size, alloc) ((size) | (alloc))
 #define PUT(p, val) (*(unsigned int *)(p) = (val)) //write 4 bytes
+#define GET(p) (*(unsigned int *)(p)) //Read content of word 
+#define GET_ALLOC(p) (*(unsigned int *)(p) & 0x1)
+#define SIZE(p) ((GET(p)) & ~0x7) //Get word value and 0 out last three bits
+
+#define HDRP(p) ((char *)(p) - WSIZE)
+#define FTRP(p) ((char *)(p) + SIZE(HDRP(p)) - 8) //retreive block size from header (-4 bytes) then add that to the ptr - 4
+#define NEXT_BLKP(p) ((char *)(p) + SIZE((char *)(p) - 4))
+#define LAST_BLKP(p) ((char *)(p) - SIZE((char *)(p) - 8)) //read footer of prev move ptr back by its size
 static char *heap_listp;
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+
+void *coalesce(void * ptr)
+{
+    size_t last_a = GET_ALLOC(HDRP(LAST_BLKP(ptr)));
+    size_t next_a = GET_ALLOC(HDRP(NEXT_BLKP(ptr)));
+
+    if(last_a == 0){
+        size_t cur_size = SIZE(HDRP(ptr));
+        size_t prev_size = SIZE(HDRP(LAST_BLKP(ptr)));
+        ptr = LAST_BLKP(ptr);
+        PUT(HDRP(ptr), PACK(cur_size + prev_size, 0));
+        PUT(FTRP(ptr), PACK(cur_size + prev_size, 0));
+    }
+    if(next_a == 0){
+        size_t cur_size = SIZE(HDRP(ptr));
+        size_t next_size = SIZE(HDRP(NEXT_BLKP(ptr)));
+        PUT(HDRP(ptr), PACK(cur_size + next_size, 0));
+        PUT(FTRP(ptr), PACK(cur_size + next_size, 0));
+    }
+
+    return ptr;
+}
 /* 
  * mm_init - initialize the malloc package.
  */
+//extend heap by words many words called (1) in init phase and (2) when no block is free
+static void *extend_heap(size_t words)
+{
+    char *bp;
+    size_t size;
+    //allocate an even num of words to keep 8 byte alignment
+    //size in bytes
+    //bp points to first byte outside of old heap
+    size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
+    if(((bp = mem_sbrk(size)) == (void *)-1)) return NULL;
+    //note: bp points to payload area of new free block, thus new header is overwriting old epilogue header
+    PUT(HDRP(bp), PACK(size, 0)); //free block hdr
+    PUT(FTRP(bp), PACK(size, 0)); //free block ftr
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1)); //new epilogue header 
+    
+    return coalesce(bp);
+
+}
 int mm_init(void)
 {
     //mem_sbrk return a pointer to -1 if something went wrong
@@ -44,7 +94,7 @@ int mm_init(void)
     PUT(heap_listp + (3*WSIZE), PACK(0, 1)); //epilogue hdr
 
     heap_listp += (2*WSIZE);
-
+    if(extend_heap(CHUNKSIZE/WSIZE) != 0) return -1;
     return 0;
 }
 
@@ -69,6 +119,10 @@ void *mm_malloc(size_t size)
  */
 void mm_free(void *ptr)
 {
+    size_t size = SIZE(HDRP(ptr));
+    PUT(HDRP(ptr), PACK(size, 0));
+    PUT(FTRP(ptr), PACK(size, 0));
+    coalesce(ptr);
 }
 
 /*
